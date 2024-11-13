0759db03bb4e0dcb4840d934141929ba2697b1da
Numbcranium <fakuadesamuel28@gmail.com>
2024-11-13 20:54:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var pi = 3.142; 
+var radius = 65;
+var Area = pi*radius**2;
+console.log(Area);
+
+var half = 1/2;
+var base = 24;
+var height = 32;
+var Area2 = half*base*height;
+console.log(Area2);
+
+var principal = 1500
+var time = 20
+var rate = 2/100
+var SimpleInterest = principal*time*rate
+console.log(SimpleInterest);
+
+var b = -65
+var num = 4;
+var a = 5;
+var c = 6;
+var num2 = 2;
+var squareRoot =  Math.sqrt(b**2-num*a*c) 
+var x = b+squareRoot/num2*a && b-squareRoot/num2*a
+console.log(x);
